4762eaa266588d1e2cec74fde2a4e60c7848a5af
Cheng Po Sheng <bencer@outlook.com>
2021-08-04 22:14:03 +0800

    mdx data layer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,26 +1,43 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import {graphql} from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({data}) => {
     return (
         <Layout pagetitle="My blog">
-        <ul>
-            {data.allFile.nodes.map(node => (
-                <li key={node.name}>{node.name}</li>
+            {data.allMdx.nodes.map(node => (
+                <article key={node.id}>
+                  <h2>{node.frontmatter.title}</h2>
+                  <p>Posted {node.frontmatter.date} </p>
+                  <p>Last modified {node.parent.mtime}</p>
+                  <MDXRenderer>
+                    { node.body }
+                  </MDXRenderer>
+                </article>
                 )
             )}
-        </ul>
         </Layout>
     )
 }
 
 export const query = graphql`query {
-    allFile {
-      nodes {
-        name
+  allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMM DD, YYYY")
+        title
       }
+      id
+      parent {
+        ... on File {
+          name
+          mtime(formatString: "A h:m:s, MMM DD, yyyy")
+        }
+      }
+      body
     }
-  }`
+  }
+}`
 
 export default BlogPage
