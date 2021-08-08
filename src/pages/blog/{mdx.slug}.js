77557cbe63fc6996d7b9b2ec2c564c1981fd9361
Cheng Po Sheng <bencer@outlook.com>
2021-08-08 22:54:52 +0800

    file system api implemented

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+
+const BlogPost = () => {
+    return (
+        <Layout pagetitle='Blog Post'>
+            <p>The content of each blog post.</p>
+        </Layout>
+    )
+}
+
+export default BlogPost
