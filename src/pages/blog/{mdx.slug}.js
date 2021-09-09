df50bbc1ca92a88067b99e08c44f60796ffbca1f
Cheng Po Sheng <bencer@outlook.com>
2021-09-09 11:29:34 +0800

    tutorial 7 completeted

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,12 +2,16 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import {GatsbyImage, getImage} from 'gatsby-plugin-image'
 
 const BlogPost = ({ data }) => {
+  const hero = getImage(data.mdx.frontmatter.hero_path)
     return (
         <Layout pagetitle={data.mdx.frontmatter.title}>
             <h2>{data.mdx.frontmatter.date}</h2>
             <p>From file {data.mdx.parent.name}.mdx modified {data.mdx.parent.modifiedTime}</p>
+            <GatsbyImage image={hero} alt={data.mdx.frontmatter.hero_path} />
+            <p>Image credit: {" "} <a href={data.mdx.frontmatter.hero_link}> Unsplash </a> </p>
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
@@ -17,20 +21,26 @@ const BlogPost = ({ data }) => {
 
 export const query = graphql`
 query MyQuery($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "DD MMM yyyy")
-      }
-      body
-      parent {
-        ... on File {
-          name
-          modifiedTime(formatString: "DD MMM yyyy")
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "DD MMM yyyy")
+      hero_link
+      hero_path {
+        childImageSharp {
+          gatsbyImageData
         }
       }
     }
+    body
+    parent {
+      ... on File {
+        name
+        modifiedTime(formatString: "DD MMM yyyy")
+      }
+    }
   }
+}
   
 `
 
